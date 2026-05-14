8b9bcb8dcccc15696de25968b119eb4e278f585a
MagicBOTAlex <zhentao2004@gmail.com>
2026-05-14 14:46:57 +0200

    font and nvi9m setup

--- setup.sh
+++ setup.sh
@@ -9,4 +9,15 @@ if [ ! -f ~/.ssh/id_ed25519 ]; then
 	ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_ed25519 -q -N ""
 fi
 
+# setup font
+
+if [ ! -f ~/.termux/font.ttf ]; then
+	curl -k https://deprived.dev/assets/CozetteVector-nerd.ttf -o ~/.termux/font.ttf
+	termux-reload-settings
+fi
+
+# setup neovim
+rm -fr ~/.config/nvim
+git clone https://github.com/MagicBOTAlex/NVimConfigs ~/.config/nvim/
+
 cat ~/.ssh/id_ed25519.pub | qrencode -t UTF8
